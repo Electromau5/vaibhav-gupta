874cf1b753d186fb983f6d17ac0eadb5c6d796fb
Pritish Sai <Pritish.sai@gmail.com>
2016-08-20 15:46:06 +0530

    first commit

---
+++ app/assets/javascripts/slider.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $('.flexslider').flexslider({
+    animation: "slide",
+    itemWidth: 700,
+    itemMargin: 5  
+});
+});
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,35 @@
+ class PagesController < ApplicationController
+  
+  def landing
+  end
+
+  def about
+  end
+
+  def resume
+  end
+
+  def project1
+  end
+
+  def project2
+  end
+  
+  def project3
+  end
+  
+  def project4
+  end
+
+  def project5
+  end
+
+  def project6
+  end
+
+  def research
+  end
+
+  def vision
+  end 
+end
