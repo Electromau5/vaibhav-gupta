0a73be5848c1fb6068b1a9117a3cd1aac077ae98
Pritish Sai <Pritish.sai@gmail.com>
2016-08-23 17:30:35 +0530

    Removed turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,6 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
 //= require masonry/jquery.masonry
 //= require masonry/jquery.event-drag
 //= require masonry/jquery.imagesloaded.min
--- app/assets/javascripts/slider.js
+++ app/assets/javascripts/slider.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(function() {
   $('.flexslider').flexslider({
     animation: "slide",
     itemWidth: 700,
